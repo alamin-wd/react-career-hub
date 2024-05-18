7f18b42165cef78061c1745886bd3e0ea780d1e6
alamin-wd <mdalamin67198@gmail.com>
2024-05-18 23:15:20 +0600

    Project set up

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+
+const Footer = () => {
+    return (
+
+        <div>
+            <footer className="footer">
+                <aside>
+                    <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+
+            </footer>
+
+            <div className="flex justify-between items-center">
+
+                <div>
+                    <p>@2024 CareerHub. All Rights Reserved</p>
+                </div>
+
+                <div>
+                    <p>Powered by CareerHub</p>
+                </div>
+
+            </div>
+
+        </div>
+
+    );
+};
+
+export default Footer;
